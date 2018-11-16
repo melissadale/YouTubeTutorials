a74f500a42826ea8db4ea5b75b337ed1a61f2fa6
Melissa <melissa.r.dale@gmail.com>
2018-11-15 21:19:40 -0500

    Add parameters to report

--- ATM - PythonMachineLearning/cookbook/generate_report.py
+++ ATM - PythonMachineLearning/cookbook/generate_report.py
@@ -1,18 +1,19 @@
 import sqlite3
 import json
 import pandas as pd
+import pickle
 
 
-experiments = ['./atm.db']  ###  path to atm's generated db - can be a list of result dbs for different experiments
-df = pd.DataFrame(columns=['experiment', 'method', 'id', 'accuracy', 'SD', 'cohen_kappa', 'f1', 'mcc', 'roc_auc', 'ap'])
+experiments = ['../atm.db']  ###  path to atm's generated db - can be a list of result dbs for different experiments
+df = pd.DataFrame(columns=['experiment', 'method', 'id', 'accuracy', 'SD', 'cohen_kappa', 'f1', 'mcc', 'roc_auc', 'ap', 'parameters'])
 
 
 def find_best(my_db):
-    df_sub = pd.DataFrame(columns=['experiment', 'method', 'id', 'accuracy', 'SD', 'cohen_kappa', 'f1', 'mcc', 'roc_auc', 'ap'])
+    df_sub = pd.DataFrame(columns=['experiment', 'method', 'id', 'accuracy', 'SD', 'cohen_kappa', 'f1', 'mcc', 'roc_auc', 'ap', 'parameters'])
     conn = sqlite3.connect(my_db)
     cur = conn.cursor()
 
-    cur.execute("SELECT classifiers.id, classifiers.metrics_location, classifiers.cv_judgment_metric, classifiers.cv_judgment_metric_stdev, "
+    cur.execute("SELECT classifiers.id, classifiers.metrics_location, classifiers.model_location, classifiers.cv_judgment_metric, classifiers.cv_judgment_metric_stdev, "
                 "hyperpartitions.method, max(classifiers.cv_judgment_metric)as best_seen FROM hyperpartitions "
                 "LEFT JOIN classifiers on classifiers.hyperpartition_id = hyperpartitions.id "
                 "GROUP BY hyperpartitions.method")
@@ -22,13 +23,17 @@ def find_best(my_db):
     for i in range(len(rows)):
         if None not in rows[i]:
             metrics = rows[i][1]
-            with open('./'+metrics) as f:  ###  path to atm's metrics
+            models = rows[i][2]
+            with open('../'+metrics) as f:  ###  path to atm's metrics
                 for line in f:
                     data=json.loads(line)
             subs = data['test']
 
+            best_model = pickle.load(open('../'+models, "rb"))
+            best_params = best_model.params
+
             df_sub.loc[i] = [my_db, rows[i][4], rows[i][0], subs['accuracy'], rows[i][3],
-                         subs['cohen_kappa'], subs['f1'], subs['mcc'], subs['roc_auc'], subs['ap']]
+                         subs['cohen_kappa'], subs['f1'], subs['mcc'], subs['roc_auc'], subs['ap'], best_params]
 
     return df_sub
 
